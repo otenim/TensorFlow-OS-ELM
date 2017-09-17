8b590e895a756b3964b72e7c8ed31f941b2e1c78
otenim <snake_istobelieve@keio.jp>
2017-09-17 19:36:14 +0900

    add

--- utils.py
+++ utils.py
@@ -6,3 +6,18 @@ def normalize_dataset(dataset, axis=None):
     min_value = np.min(dataset, axis=axis)
     normalized = (dataset - min_value) / (max_value - min_value)
     return normalized
+
+def dump_dataset(path, dataset):
+    # dataset assumes 2D array
+    dim = len(dataset.shape)
+    with open(path, 'w') as f:
+        if dim == 2:
+            for i in range(len(dataset)):
+                for j in range(len(dataset[0])):
+                    f.write('%f ' % dataset[i][j])
+                f.write('\n')
+        elif dim == 1:
+            for i in range(len(dataset)):
+                f.write('%f\n' % dataset[i])
+        else:
+            Exception('Dimension of the input array must be 1 or 2')
