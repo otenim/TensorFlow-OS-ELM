ed1460179c07dbc1a9ffdffcb8018bf8b25c47ee
otenim <snake_istobelieve@keio.jp>
2017-11-18 20:23:45 +0900

    fixed bug

--- datasets.py
+++ datasets.py
@@ -7,6 +7,8 @@ def load_mnist():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.astype(np.float32) / 255.
     x_test = x_test.astype(np.float32) / 255.
+    x_train = x_train.reshape(-1,28**2)
+    x_test = x_test.reshape(-1,28**2)
     y_train = to_categorical(y_train.astype(np.float32), num_classes)
     y_test = to_categorical(y_test.astype(np.float32), num_classes)
     return (x_train, y_train), (x_test, y_test)
