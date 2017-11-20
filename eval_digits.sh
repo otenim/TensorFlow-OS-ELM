912fbc7dbf8b15e1b99d30c28e59aab1a83ddfab
otenim <snake_istobelieve@keio.jp>
2017-11-20 19:01:37 +0900

    minor change

--- eval_digits.sh
+++ eval_digits.sh
@@ -3,11 +3,12 @@
 activation="sigmoid"
 loss="mean_squared_error"
 dataset="digits"
+result_dir="./results"
 
 for batch_size in 4 8 16 32 64
 do
     for units in 32 64 128 256 512
     do
-        python train.py --dataset $dataset --units $units --batch_size $batch_size --activation $activation --loss $loss
+        python train.py --dataset $dataset --units $units --batch_size $batch_size --activation $activation --loss $loss --result $result_dir/$dataset
     done
 done
