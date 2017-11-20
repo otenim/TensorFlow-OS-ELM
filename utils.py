63558a921f765c957a00df08915833869c27aa51
otenim <snake_istobelieve@keio.jp>
2017-11-20 18:00:53 +0900

    minor change

--- utils.py
+++ utils.py
@@ -13,8 +13,9 @@ def z_normalize(x, axis=None):
     return (x - mean)/std
 
 def save_data(data, result_dir):
+    dataset = data['dataset']
     units = data['units']
     batch_size = data['batch_size']
-    fname = 'units%d_bsize%d.pkl' % (units, batch_size)
+    fname = '%s_units%d_bsize%d.pkl' % (dataset, units, batch_size)
     with open(os.path.join(result_dir, fname), 'wb') as f:
         pkl.dump(data, f)
