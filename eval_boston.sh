f5538454a8a86ac2b1e6862ccae64127dd17ae8f
otenim <snake_istobelieve@keio.jp>
2017-11-22 04:55:36 +0900

    edited

--- eval_boston.sh
+++ eval_boston.sh
@@ -4,11 +4,12 @@ activation="sigmoid"
 loss="mean_squared_error"
 dataset="boston"
 result_dir="./results/${dataset}"
+model_dir="./models/speed/${dataset}"
 
 for batch_size in 4 8 16 32 64
 do
     for units in 8 16 32 64
     do
-        python train.py --dataset $dataset --units $units --batch_size $batch_size --activation $activation --loss $loss --result $result_dir
+        python train.py --dataset $dataset --units $units --batch_size $batch_size --activation $activation --loss $loss --result_dir $result_dir --model_dir $model_dir
     done
 done
