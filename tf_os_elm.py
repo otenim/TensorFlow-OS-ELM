ff56a427b6d9fd31e3f997579aa706a270eb7e05
otenim <snake_istobelieve@keio.jp>
2018-03-13 20:24:29 +0900

    added

--- tf_os_elm.py
+++ tf_os_elm.py
@@ -5,75 +5,120 @@ import tensorflow as tf
 class OS_ELM(object):
 
     def __init__(self, n_input_nodes, n_hidden_nodes, n_output_nodes):
+        self.__sess = tf.Session()
+        self.__is_finished_init_train = False
         self.__n_input_nodes = n_input_nodes
         self.__n_hidden_nodes = n_hidden_nodes
         self.__n_output_nodes = n_output_nodes
         self.__activation = tf.nn.sigmoid
-        self.__loss = tf.losses.mean_squared_error
-        self.__x = tf.placeholder(tf.float32, [None, self.__n_input_nodes])
-        self.__y = tf.placeholder(tf.float32, [None, self.__n_output_nodes])
+        self.__x = tf.placeholder(tf.float32, [None, self.__n_input_nodes], name='input_x')
+        self.__t = tf.placeholder(tf.float32, [None, self.__n_output_nodes], name='input_t')
         self.__alpha = tf.constant(
-            tf.random_uniform(
-                [self.__n_input_nodes, self.__n_hidden_nodes],
-                minval=-1.0,
-                maxval=1.0,
-            )
+            np.random.uniform(-1,1,size=(self.__n_input_nodes, self.__n_hidden_nodes)),
+            dtype=tf.float32,
+            name='alpha',
         )
-        self.__beta = tf.Variable(
-            tf.random_uniform(
-                [self.__n_hidden_nodes, self.__n_output_nodes],
-                minval=-1.0,
-                maxval=1.0,
-            ),
-            trainable=False,
+        self.__bias = tf.constant(
+            np.zeros(shape=(self.__n_hidden_nodes)),
+            dtype=tf.float32,
+            name='bias',
         )
         self.__p = tf.Variable(
-            tf.zeros([self.__n_hidden_nodes, self.__n_hidden_nodes]),
+            initial_value=np.zeros(shape=(self.__n_hidden_nodes, self.__n_hidden_nodes)),
             trainable=False,
+            dtype=tf.float32,
+            name='p',
         )
-        self.__bias = tf.constant(tf.zeros([self.__n_hidden_nodes]))
+        self.__beta = tf.Variable(
+            initial_value=np.zeros(shape=(self.__n_hidden_nodes, self.__n_output_nodes)),
+            trainable=False,
+            dtype=tf.float32,
+            name='beta',
+        )
 
-        # graph for initial training phase
+        # Predict
+        self.__predict = self.__activation(tf.matmul(self.__x, self.__alpha) + self.__bias)
+
+        # Initial training phase
         self.__init_train_p, self.__init_train_beta = self.__build_init_train_graph()
-        # graph for sequential training phase
+
+        # Sequential training phase
         self.__seq_train_p, self.__seq_train_beta = self.__build_seq_train_graph()
 
+        # Initialize variables
+        self.__sess.run(tf.initialize_variables([self.__p, self.__beta]))
+
+    def predict(self, x):
+        return self.__sess.run(self.__predict, feed_dict={self.__x: x})
+
+    def fit(self, x, t, batch_size=16, verbose=1):
+
+        if verbose:
+            pbar = tqdm.tqdm(total=len(x))
+
+        if self.__is_finished_init_train == False:
+            if verbose:
+                pbar.set_description('initial training phase')
+            if len(x) < self.__n_hidden_nodes:
+                raise ValueError(
+                    'If this is the first time for you to train the model, '
+                    'the number of training samples '
+                    'must be greater than the number of hidden nodes.'
+                )
+            x_init = x[:self.__n_hidden_nodes]
+            t_init = t[:self.__n_hidden_nodes]
+            self.__sess.run(self.__init_train_p, feed_dict={self.__x: x_init})
+            self.__sess.run(self.__init_train_beta, feed_dict={self.__t: t_init})
+            self.__is_finished_init_train = True
+            if verbose:
+                pbar.update(n=len(x_init))
+            x_seq = x[self.__n_hidden_nodes:]
+            t_seq = t[self.__n_hidden_nodes:]
+        else:
+            x_seq = x
+            t_seq = t
+
+        if verbose:
+            pbar.set_description('sequential training phase')
+        for i in range(0, len(x_seq), batch_size):
+            x_batch = x_seq[i:i+batch_size]
+            t_batch = t_seq[i:i+batch_size]
+            self.__sess.run(self.__seq_train_p, feed_dict={self.__x: x_batch})
+            self.__sess.run(self.__seq_train_beta, feed_dict={self.__t: t_batch})
+            if verbose:
+                pbar.update(n=len(x_batch))
+
+        if verbose:
+            pbar.close()
 
     def __build_init_train_graph(self):
         H = self.__activation(tf.matmul(self.__x, self.__alpha) + self.__bias)
         HT = tf.transpose(H)
-
-        # update p
-        HTH = tf.matmul(H, HT)
-        init_train_p = self.__p.assign(tf.matrix_inverse(HTH))
-
-        # update beta
+        HTH = tf.matmul(HT, H)
+        init_train_p = tf.assign(self.__p, tf.matrix_inverse(HTH))
         pHT = tf.matmul(self.__p, HT)
-        pHTy = tf.matmul(pHT, self.__y)
-        init_train_beta = self.__beta.assign(pHTy)
+        pHTt = tf.matmul(pHT, self.__t)
+        init_train_beta = tf.assign(self.__beta, pHTt)
         return (init_train_p, init_train_beta)
 
     def __build_seq_train_graph(self):
         H = self.__activation(tf.matmul(self.__x, self.__alpha) + self.__bias)
         HT = tf.transpose(H)
+        HTH = tf.matmul(HT, H)
         batch_size = tf.shape(self.__x)[0]
         I = tf.eye(batch_size)
-
-        # update p
         Hp = tf.matmul(H, self.__p)
         HpHT = tf.matmul(Hp, HT)
-        IHpHT = tf.matrix_inverse(I + HpHT)
+        temp = tf.matrix_inverse(I + HpHT)
         pHT = tf.matmul(self.__p, HT)
-        pHTIHpHT = tf.matmul(pHT, IHpHT)
-        pHTIHpHTH = tf.matmul(pHTIHpHT, H)
-        pHTIHpHTHp = tf.matmul(pHTIHpHTH, self.__p)
-        seq_train_p = self.__p.assign(self.__p - pHTIHpHTHp)
-
-        # update beta
+        seq_train_p = tf.assign(self.__p, self.__p - tf.matmul(tf.matmul(pHT, temp), Hp))
         pHT = tf.matmul(self.__p, HT)
         Hbeta = tf.matmul(H, self.__beta)
-        seq_train_beta = self.__beta.assign(tf.matmul(pHT, self.__y - Hbeta))
+        seq_train_beta = tf.assign(self.__beta, self.__beta + tf.matmul(pHT, self.__t - Hbeta))
         return (seq_train_p, seq_train_beta)
+
+    def __del__(self):
+        self.__sess.close()
 
     @property
     def n_input_nodes(self):
---
+++ tf_train_mnist.py
@@ -0,0 +1,106 @@
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from tf_os_elm import OS_ELM
+import numpy as np
+import argparse
+import tensorflow as tf
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_hidden_nodes', type=int, default=1024)
+parser.add_argument('--batch_size', type=int, default=32)
+
+def main(args):
+
+    # ===========================================
+    # Prepare dataset
+    # ===========================================
+    n_input_dimensions = 784
+    n_classes = 10
+
+    (x_train, t_train), (x_test, t_test) = mnist.load_data()
+    x_train = x_train.reshape(-1, n_input_dimensions) / 255.
+    x_test = x_test.reshape(-1, n_input_dimensions) / 255.
+    x_train = x_train.astype(np.float32)
+    x_test = x_test.astype(np.float32)
+
+    t_train = to_categorical(t_train, num_classes=n_classes)
+    t_test = to_categorical(t_test, num_classes=n_classes)
+    t_train = t_train.astype(np.float32)
+    t_test = t_test.astype(np.float32)
+
+    # ===========================================
+    # Instantiate os-elm
+    # ===========================================
+    print(x_train.shape)
+    print(x_test.shape)
+    print(t_train.shape)
+    print(t_test.shape)
+    os_elm = OS_ELM(
+        # number of nodes of input layer
+        n_input_nodes=n_input_dimensions,
+        # number of nodes of hidden layer
+        n_hidden_nodes=args.n_hidden_nodes,
+        # number of nodes of output layer
+        n_output_nodes=n_classes, # 10
+    )
+
+
+    # ===========================================
+    # training
+    # ===========================================
+    os_elm.fit(x_train, t_train, batch_size=args.batch_size, verbose=1)
+    sess.close()
+
+    """
+    # ===========================================
+    # Prediction
+    # ===========================================
+    # NOTE: input numpy arrays' shape is always assumed to
+    # be in the following 2D format: (batch_size, n_input_nodes).
+    # Even when you feed one training sample to the model,
+    # the input sample's shape must be (1, n_input_nodes), not
+    # (n_input_nodes,). Here, we feed one validation sample
+    # as an example.
+    n = 5
+    x = x_test[:n]
+    y_pred = os_elm.predict(x, softmax=True)
+
+    for i in range(n):
+        print("========== Prediction result (%d) ==========" % i)
+        class_id = np.argmax(y_pred[i])
+        print("class_id (prediction): %d" % class_id)
+        print("class_id (true): %d" % np.argmax(t_test[i]))
+        class_prob = y_pred[i][class_id]
+        print("probability (prediction): %.3f" % class_prob)
+
+    # ===========================================
+    # Evaluation
+    # ===========================================
+    print("========== Evaluation result ==========")
+    loss, acc = os_elm.evaluate(
+        x_test,
+        t_test,
+        # 'loss' and 'accuracy' are supported so far.
+        # the default value is ['loss']
+        # NOTE: 'accuracy' is only available for classification problems.
+        metrics=['loss', 'accuracy']
+    )
+    print('validation loss: %f' % loss)
+    print('classification accuracy: %f' % acc)
+
+    # ===========================================
+    # Save model
+    # ===========================================
+    print("saving trained model as model.pkl...")
+    os_elm.save('model.pkl')
+
+    # ===========================================
+    # Load model
+    # ===========================================
+    print('loading model froom model.pkl...')
+    os_elm = load_model('model.pkl')
+    """
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
