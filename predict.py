7c57f75320adcdafa0ab363d8f113ca36cc9e734
otenim <snake_istobelieve@keio.jp>
2018-03-14 06:31:13 +0900

    add

---
+++ predict.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import tqdm
+import time
+import numpy as np
+from os_elm import OS_ELM
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_input_nodes', type=int, default=784)
+parser.add_argument('--n_hidden_nodes', type=int, default=32)
+parser.add_argument('--n_output_nodes', type=int, default=784)
+parser.add_argument('--loss', default='mean_absolute_error')
+parser.add_argument('--activation', default='linear')
+parser.add_argument('--batch_size', type=int, default=1)
+parser.add_argument('--n', type=int, default=10000)
+
+def main(args):
+
+    os_elm = OS_ELM(
+        n_input_nodes=args.n_input_nodes,
+        n_hidden_nodes=args.n_hidden_nodes,
+        n_output_nodes=args.n_output_nodes,
+        loss=args.loss,
+        activation=args.activation,
+    )
+
+    pbar = tqdm.tqdm(total=args.n)
+    times = []
+    for i in range(args.n):
+        x = np.random.normal(size=(args.batch_size, args.n_input_nodes))
+        t = np.random.normal(size=(args.batch_size, args.n_output_nodes))
+        stime = time.time()
+        os_elm.evaluate(x, t)
+        etime = time.time()
+        times.append(etime - stime)
+        pbar.update(1)
+    times = np.array(times)
+    mean = np.mean(times)
+    print('mean prediction time: %f [msec/batch]' % (1000*mean))
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
