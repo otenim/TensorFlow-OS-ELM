70c567b879c0028cead80f1e307bb5da820f3ecf
otenim <snake_istobelieve@keio.jp>
2018-03-16 08:21:45 +0900

    bug fixed

--- predict.py
+++ predict.py
@@ -11,7 +11,6 @@ parser.add_argument('--n_hidden_nodes', type=int, default=32)
 parser.add_argument('--n_output_nodes', type=int, default=784)
 parser.add_argument('--loss', default='mean_absolute_error')
 parser.add_argument('--activation', default='linear')
-parser.add_argument('--batch_size', type=int, default=1)
 parser.add_argument('--n', type=int, default=10000)
 
 def main(args):
@@ -24,19 +23,21 @@ def main(args):
         activation=args.activation,
     )
 
-    pbar = tqdm.tqdm(total=args.n)
-    times = []
-    for i in range(args.n):
-        x = np.random.normal(size=(args.batch_size, args.n_input_nodes))
-        t = np.random.normal(size=(args.batch_size, args.n_output_nodes))
-        stime = time.time()
-        os_elm.evaluate(x, t)
-        etime = time.time()
-        times.append(etime - stime)
-        pbar.update(1)
-    times = np.array(times)
-    mean = np.mean(times)
-    print('mean prediction time: %f [msec/batch]' % (1000*mean))
+    for batch_size in [4, 8, 16, 32, 64, 128, 256]:
+        pbar = tqdm.tqdm(total=args.n, desc='Batchsize %d' % batch_size)
+        times = []
+        for i in range(args.n):
+            x = np.random.normal(size=(batch_size, args.n_input_nodes))
+            t = np.random.normal(size=(batch_size, args.n_output_nodes))
+            stime = time.time()
+            os_elm.evaluate(x, t)
+            etime = time.time()
+            times.append(etime - stime)
+            pbar.update(1)
+        pbar.close()
+        times = np.array(times)
+        mean = np.mean(times)
+        print('mean prediction time: %f [msec/batch]' % (1000*mean))
 
 if __name__ == '__main__':
     args = parser.parse_args()
