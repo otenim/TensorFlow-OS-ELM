efcb039089d68794a8a9a6fd4d6b9b30f9233825
otenim <snake_istobelieve@keio.jp>
2018-03-16 09:30:57 +0900

    first

---
+++ train_nn.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+import tqdm
+import time
+import numpy as np
+from nn import NN
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_input_nodes', type=int, default=784)
+parser.add_argument('--n_hidden_nodes', type=int, default=32)
+parser.add_argument('--n_output_nodes', type=int, default=784)
+parser.add_argument('--loss', default='mean_absolute_error')
+parser.add_argument('--hidden_activation', default='relu')
+parser.add_argument('--output_activation', default='sigmoid')
+parser.add_argument('--n', type=int, default=10000)
+
+def main(args):
+
+    model = NN(
+        n_input_nodes=args.n_input_nodes,
+        n_hidden_nodes=args.n_hidden_nodes,
+        n_output_nodes=args.n_output_nodes,
+        loss=args.loss,
+        hidden_activation=args.hidden_activation,
+        output_activation=args.output_activation,
+    )
+
+    for batch_size in [4, 8, 16, 32, 64, 128, 256]:
+        pbar = tqdm.tqdm(total=args.n, desc='Batchsize %d' % batch_size)
+        times = []
+        for i in range(args.n):
+            x = np.random.uniform(size=(batch_size, args.n_input_nodes))
+            stime = time.time()
+            model.test_on_batch(x, x)
+            etime = time.time()
+            times.append(etime - stime)
+            pbar.update(1)
+        pbar.close()
+        times = np.array(times)
+        mean = np.mean(times)
+        print('mean training time: %f [msec/batch]' % (1000*mean))
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
